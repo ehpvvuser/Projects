eb0c4eb5178be6a842db9ac3485f0b324129555d
ehpvvuser <ehpvvuser@gmail.com>
2025-07-17 11:25:39 +0530

    Update HP.java

--- HP.java
+++ HP.java
@@ -1,7 +1,7 @@
 public class HP{
 public static void main(String[] args)
 	System.out.println("Hi");
-	System.out.println("Hello");
+	System.out.println("Hello hai");
 System.out.println("welcome");
 }
 }
